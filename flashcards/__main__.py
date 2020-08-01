8ccdbf6fa362985cb6c825a5ac14e75a88837fde
JJ Style <style.jj@pm.me>
2020-08-01 20:52:31 +0100

    fix main entry point

--- flashcards/__main__.py
+++ flashcards/__main__.py
@@ -2,7 +2,7 @@ import sys
 import argparse
 from flashcards.card_set import Set, learn_set
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--shuffle", help="shuffle order of flashcards in the set", action="store_true")
     parser.add_argument("-r", "--reverse", help="reverse answering, answer with term rather than definition", action="store_true")
@@ -12,3 +12,6 @@ if __name__ == "__main__":
 
     card_set = Set.load_from_csv(args.path)
     learn_set(card_set, shuffle=args.shuffle, answer_reverse=args.reverse)
+
+if __name__ == "__main__":
+    main()
